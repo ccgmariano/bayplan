c1fdd51a47e25b463fbeee4f4576a807645cd76d
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2026-01-05 02:46:24 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,14 @@ import { fileURLToPath } from "url";
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 
 // helpers p/ ler SQL
 const __filename = fileURLToPath(import.meta.url);
